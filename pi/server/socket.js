5ec8ee4d493e07e418135460c2f771811ca8f579
Jacob Cunningham <jcun4128@gmail.com>
2020-10-28 08:03:23 +0000

    working socket to python call

--- pi/server/socket.js
+++ pi/server/socket.js
@@ -2,6 +2,7 @@
 
 const http = require('http');
 const WebSocketServer = require('websocket').server;
+const exec = require('child_process').exec;
 
 const server = http.createServer();
 server.listen(3000);
@@ -14,7 +15,21 @@ wsServer.on('request', function(request) {
   const connection = request.accept(null, request.origin);
 
   connection.on('message', function(message) {
-    console.log('socket received:', message);
+    message.utf8Data
+    // this shouldn't be hardcoded but depends on how installed
+    // from repo as is this would be the path if cloning into /home/pi
+    const msgParts = message.utf8Data.split('_');
+    const cmd = `cd /home/pi/fusion-sensor-clu/pi/cli-commands
+    python move-servo.py ${msgParts[0]} ${msgParts[1]}`;
+
+    exec(cmd, function (error, stdout, stderr) {
+      if (error) {
+	console.log('command failed', error.message);
+      }
+      if (stderr) {
+	console.log('stderr', stderr);
+      }
+    });
   });
 
   // connection.on('close', function(reasonCode, description)) {
