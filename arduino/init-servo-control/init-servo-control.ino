2388ca1c28c888ed83865312f6d4be29f77afb1a
Jacob Cunningham <jcun4128@gmail.com>
2020-11-02 20:07:42 -0600

    working on sweep code, updated command string parser

--- arduino/init-servo-control/init-servo-control.ino
+++ arduino/init-servo-control/init-servo-control.ino
@@ -15,8 +15,10 @@ int panMaxRange = 30;
 int tiltServoPos = 89;
 int tiltMaxRange = 30;
 
-String moveServo = ""; // p or t for pan/tilt
-String moveServoPos = ""; // 0-180 casted to int
+String mainCmd = "";
+String secondCmd = "";
+String thirdCmd = "";
+String fourthCmd = "";
 
 void setup() {
   Serial.begin(9600);
@@ -32,13 +34,19 @@ void setup() {
 void servoWrite(String servo, int pos) {
   if (servo == "pan") {
     if (pos <= panServoPos + panMaxRange || pos >= panServoPos - panMaxRange) {
-      panServo.write(pos);
+      // panServo.write(pos);
+      Serial.println("pan servo");
+      Serial.print(pos);
     }
   } else {
     if (pos <= tiltServoPos + tiltMaxRange || pos >= tiltServoPos - tiltMaxRange) {
-      tiltServo.write(pos);
+      // tiltServo.write(pos);
+      Serial.println("tilt servo");
+      Serial.print(pos);
     }
   }
+
+  Serial.println("");
 }
 
 void sweep(String servo, int increment = 2, int delayMs = 1000) {
@@ -59,29 +67,55 @@ void sweep(String servo, int increment = 2, int delayMs = 1000) {
 void receiveEvent(int bytes) {
   String fullCommand = "";
   int loopCounter = 0;
-  moveServo = "";
-  moveServoPos = "";
+  mainCmd = "";
+  secondCmd = "";
+  thirdCmd = "";
+  fourthCmd = "";
   
+  // the reason these values are split is because
+  // if I just join them into one big string, try to parse/access by array
+  // the values aren't correct
+  Serial.println("receiving");
   while (Wire.available()) {
     char c = Wire.read();
 
+    Serial.print(c);
+
     if (loopCounter == 1) {
-      moveServo = c;
+      mainCmd = c;
     }
 
-    if (loopCounter > 1 and loopCounter < 5) {
-      moveServoPos += c;
+    if (loopCounter > 1 and loopCounter < 5) { // eg. p179 or t179
+      secondCmd += c; // 001 for increment 1
+    }
+
+    if (loopCounter == 5) { // eg. s179t || s179p
+      thirdCmd = c;
+    }
+
+    if (loopCounter > 5) { // eg. s179t1000... what about 0.5 or 10000 (10 seconds)
+      fourthCmd += c;
     }
 
     loopCounter++;
   }
 
-  if (moveServo == "p") {
-    panServo.write(moveServoPos.toInt());
+  Serial.println("");
+
+  if (mainCmd == "p") {
+    servoWrite("pan", secondCmd.toInt());
   }
   
-  if (moveServo == "t") {
-    tiltServo.write(moveServoPos.toInt());
+  if (mainCmd == "t") {
+    servoWrite("tilt", secondCmd.toInt());
+  }
+
+  if (mainCmd == "s") {
+    if (thirdCmd == "p") {
+      sweep("pan", secondCmd.toInt(), fourthCmd.toInt());
+    } else {
+      sweep("tilt", secondCmd.toInt(), fourthCmd.toInt());
+    }
   }
 }
 
